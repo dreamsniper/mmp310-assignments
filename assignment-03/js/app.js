66be05427b87f7110e5f84b46a3b68fc6f8793f3
Steven Taveras <thedreamsniper@gmail.com>
2017-03-01 17:34:05 -0500

    Added functionality to the buttons with javascript to test the buttons from class example

--- assignment-03/js/app.js
+++ assignment-03/js/app.js
@@ -1,2 +1,39 @@
 /*jslint browser, es6, single, for, devel, this */
 /*global window */
+
+var enlargeButton = document.getElementById('enlarge-div');
+var shrinkButton = document.getElementById('shrink-div');
+
+//function showEnlargeAlert(){
+//    'use strict';
+//    
+//    alert('enlarge-div');
+//}
+//
+//function showShrinkAlert(){
+//    'use strict';
+//    
+//    alert('shrink-div');
+//}
+
+function showAlert() {
+    'use strict';
+    
+    alert('You Clicked The ' + this.id);
+}
+
+if (null !== enlargeButton) {
+    
+    enlargeButton.addEventListener('click', showAlert, false);
+
+}  else {
+        console.error('No reference to ID %c enlarge-div established');
+    }
+
+if (null !== shrinkButton) {
+    
+    shrinkButton.addEventListener('click', showAlert, false);
+
+}  else {
+        console.error('No reference to ID %c shrink-div established');
+    }
