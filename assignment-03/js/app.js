e12c50b0cbe8c2213bffa5f4208fcff31a93d19d
Steven Taveras <thedreamsniper@gmail.com>
2017-03-02 21:40:39 -0500

    beautified and created function to enlarge the the div DOM element

--- assignment-03/js/app.js
+++ assignment-03/js/app.js
@@ -22,6 +22,20 @@ function showAlert() {
     alert('You Clicked The ' + this.id);
 }
 
+//To enlarve div box by 100 px
+function enlargeBox() {
+    var box = document.querySelector('div'); //selects the div in the Dom
+    var boxWidth = window.getComputedStyle(box, null).getPropertyValue('width'); //get the width element
+    var boxHeight = window.getComputedStyle(box, null).getPropertyValue('height');
+    var newWidth = parseInt(boxWidth, 10); //gets rid of the px and leaves integer set to variable
+    var newHeight = parseInt(boxHeight, 10);
+    newWidth += 100; //adds 100px to element
+    newHeight += 100;
+    box.style.width = (newWidth) + "px"; //sets the style of the elemet
+    box.style.height = (newHeight) + "px";
+    //alert(box.style.width);
+}
+
 if (null !== enlargeButton) {
 
     enlargeButton.addEventListener('click', showAlert, false);
