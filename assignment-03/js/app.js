75f5ec6ab13868de3d14915126b0084855a1bd03
Steven Taveras <thedreamsniper@gmail.com>
2017-03-02 21:33:16 -0500

    Beautified code

--- assignment-03/js/app.js
+++ assignment-03/js/app.js
@@ -1,8 +1,8 @@
 /*jslint browser, es6, single, for, devel, this */
 /*global window */
 
-var enlargeButton = document.getElementById('enlarge-div');
-var shrinkButton = document.getElementById('shrink-div');
+var enlargeButton = document.getElementById('enlarge-div'),
+    shrinkButton = document.getElementById('shrink-div');
 
 //function showEnlargeAlert(){
 //    'use strict';
@@ -18,22 +18,22 @@ var shrinkButton = document.getElementById('shrink-div');
 
 function showAlert() {
     'use strict';
-    
+
     alert('You Clicked The ' + this.id);
 }
 
 if (null !== enlargeButton) {
-    
+
     enlargeButton.addEventListener('click', showAlert, false);
 
-}  else {
-        console.error('No reference to ID %c enlarge-div established');
-    }
+} else {
+    console.error('No reference to ID %c enlarge-div established');
+}
 
 if (null !== shrinkButton) {
-    
+
     shrinkButton.addEventListener('click', showAlert, false);
 
-}  else {
-        console.error('No reference to ID %c shrink-div established');
-    }
+} else {
+    console.error('No reference to ID %c shrink-div established');
+}
