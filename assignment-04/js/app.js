c9c56be98d9c6b5d2cd3562ced5aa30c67a5abf1
Steven Taveras <thedreamsniper@gmail.com>
2017-03-15 10:33:24 -0400

    created app.js tor assignment-04

---
+++ assignment-04/js/app.js
@@ -0,0 +1,12 @@
+// window.onLoad = function () {
+//   'use strict'
+
+//   var msg = document.getElementById('msg');
+//   msg.innerHtml = '<h1>hello, world</h1>';
+
+//   var div = document.createElement('div');
+//   var text = document.createTextNode('This is content in the new div.');
+//   div.appendChild(text);
+//   document.body.appendChild(div);
+// };
+var button = document.getElementByTagName('h2');
