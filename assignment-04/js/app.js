6fbcfa1ae21711354f2fbc1f6fb06283df4624a6
Steven Taveras <dreamsniper@users.noreply.github.com>
2017-03-23 16:34:53 -0400

    updated for more functionality
    originally had it set up simpler. After adding the classes the divs would not open again or vice versa if i changed the function. reverting back to this state seeing as others have done a similar thing with greater results. not sure where i went wrong. will continue working on this.

--- assignment-04/js/app.js
+++ assignment-04/js/app.js
@@ -7,21 +7,27 @@ var accordionButton = document.getElementsByTagName('h2');
 //add eventListeners to h2 buttons
 for (var i = 0; i < accordionButton.length; i++) {
     accordionButton[i].addEventListener('click', toggle, false);
-
-    for (var b = 0; b < 1; b++) {
-        accordionButton[b].nextElementSibling.classList = 'box show';
-    }
-    accordionButton[i].nextElementSibling.classList = 'box hide';
 }
 
+//add classes to the divs keeping first one open
+for (var j = 0; j < accordionButton.length; j++ ){
+   for (var b = 0; b < 1; b++) {
+     accordionButton[b].nextElementSibling.classList = 'box show';
+   }
+   accordionButton[j].nextElementSibling.classList = 'box hide';
+ }
+ 
+var boxSlider = document.getElementsByClassName('box');
 
 //to open and close child divs
 function toggle() {
     var currentClass = this.parentNode.className;
 
-    for (var d = 0; d < this.length; d++) {
-        if (accordionButton.nextElementSibling.className === 'box hide') {
-            accordionButton.nextElementSibling.className = 'box show';
-        }
+    for (var a = 0; a < boxSlider.length; a++) {
+        boxSlider[a].className = 'box hide';
+    }
+    if (currentClass == 'box hide') {
+        this.parentNode.className = 'box show';
+
     }
 }
