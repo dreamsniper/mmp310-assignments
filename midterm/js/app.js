20f31458a8c851adad78ab06d96168d380456226
Steven Taveras <thedreamsniper@gmail.com>
2017-04-14 09:55:32 -0400

    modified headerNav function to include all nested ul menus

--- midterm/js/app.js
+++ midterm/js/app.js
@@ -1,7 +1,7 @@
 /*jslint browser, es6, single, devel, multivar */
 /*global window */
 
-let body = document.body,
+var body = document.body,
     //0 selects the first indexed item in the array lists of the tag names
     head = document.getElementsByTagName('head')[0],
     header = document.getElementsByTagName('header')[0],
@@ -15,12 +15,54 @@ body.appendChild(script);
 
 //define HeaderNav as function loop through the file and build the menu items
 function headerNav(navItems) {
-    //create ad append a ul element
-    var ul = document.createElement('ul');
-    nav.appendChild(ul);
+    //to build the html string
+    var html = [];
 
-    //  <li><a href="#">Work</a></li>
+    //create a ul element
+    var ul = '<ul>';
+
+    //loop to populate navigation
     for (var i = 0; i < navItems.items.length; i++) {
-        ul += '<li><a href="' + navItems.items[i].url + '">' + navItems.items[i].label + '</a></li>';
+
+        if (navItems.items[i].items !== '') {
+            ul += '<li>';
+
+            //cycle through the urls and labels and build the a tag
+            ul += '<a href="' + navItems.items[i].url + '" >' + navItems.items[i].label + '</a>';
+            console.log(ul);
+
+            ul += '<ul>';
+            console.log(ul);
+            for (var a = 0; a < navItems.items[i].items.length; a++) {
+                ul += '<li>';
+                console.log(ul);
+                //cycle through theurls and labels and build the a tag
+                html[a] = '<a href="' + navItems.items[1].items[a].url + '" >' + navItems.items[1].items[a].label + '</a>';
+
+                //add ul li list
+                ul += html[a];
+                ul += '</li>';
+                console.log(ul);
+            }
+            //close the nested ul
+            ul += '</ul>';
+
+        } else {
+            //create li element
+            ul += '<li>';
+
+            //cycle through theurls and labels and build the a tag
+            html[i] = '<a href="' + navItems.items[i].url + '" >' + navItems.items[i].label + '</a>';
+
+            //add ul li list
+            ul += html[i];
+            ul += '</li>';
+        }
     }
+    //append html to nav
+    nav.innerHTML = ul;
+}
+
+function content(info) {
+
 }
