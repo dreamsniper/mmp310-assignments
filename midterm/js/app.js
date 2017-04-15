714f440c18b03c41b2e53dc52fdc16e9949a6a0b
Steven Taveras <thedreamsniper@gmail.com>
2017-04-15 11:00:16 -0400

    added variables and code for content function

--- midterm/js/app.js
+++ midterm/js/app.js
@@ -1,21 +1,33 @@
 /*jslint browser, es6, single, devel, multivar */
 /*global window */
 
+'use strict'
+
 var body = document.body,
     //0 selects the first indexed item in the array lists of the tag names
     head = document.getElementsByTagName('head')[0],
     header = document.getElementsByTagName('header')[0],
     nav = document.getElementsByTagName('nav')[0],
-    main = document.getElementsByTagName('main')[0];
+    main = document.getElementsByTagName('main')[0],
+    h1 = document.getElementsByTagName('h1')[0],
+    h2 = document.getElementsByTagName('h2'),
+    h3 = document.getElementsByTagName('h3'),
+    pTag = document.getElementsByTagName('p')[0],
+    blockquote = document.getElementsByTagName('blockquote'),
+    footer = document.getElementsByTagName('footer');
+
 
 //create script tag for the Json file and append to head
-var script = document.createElement('script');
+var script = document.createElement('script'),
+    scriptContent = document.createElement('script');
+
+scriptContent.src = 'data/content.json';
 script.src = "data/nav.json";
 body.appendChild(script);
+body.appendChild(scriptContent);
 
-//define HeaderNav as function loop through the file and build the menu items
+//define HeaderNav as function loop through the Json file and build the menu items
 function headerNav(navItems) {
-    'use strict'
 
     //to build the html string
     var html = [];
@@ -40,7 +52,6 @@ function headerNav(navItems) {
                 //cycle through theurls and labels and build the a tag
                 html[a] = '<a href="' + navItems.items[1].items[a].url + '" >' + navItems.items[1].items[a].label + '</a>';
 
-                //add ul li list
                 ul += html[a];
 
                 ul += '</li>';
@@ -49,13 +60,11 @@ function headerNav(navItems) {
             ul += '</ul>';
 
         } else {
-            //create li element
             ul += '<li>';
 
             //cycle through theurls and labels and build the a tag
             html[i] = '<a href="' + navItems.items[i].url + '" >' + navItems.items[i].label + '</a>';
 
-            //add ul li list
             ul += html[i];
 
             ul += '</li>';
@@ -66,5 +75,18 @@ function headerNav(navItems) {
 }
 
 function content(info) {
+    'use strict';
+
+    //append header
+    var header1 = document.createTextNode(info.h1);
+    h1.appendChild(header1);
+
+    //append p element
+    var pTagText = '' + info.p;
+    console.log(pTagText);
+    pTag.innerHTML(pTagText);
+
+
+
 
 }
